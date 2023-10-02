53a7f3080f7ada6aceff535d9960f854df09977c
NadiaArroyo25 <niac2596@gmail.com>
2023-10-01 20:24:54 -0600

    horroresyboton
    errores de ortografia y quitamos el button onclick

--- Convertidor.js
+++ Convertidor.js
@@ -20,9 +20,11 @@ imprimir(celsius,farenheit);
 function imprimir(celsius, farenheit) {
     document.getElementById("Resultado").textContent = `${farenheit.toFixed(2)}°F equivale a ${celsius.toFixed(2)}°C`;
 }
-
-// function read (){  
-// //     const celsius = parseFloat(document.getElementById("Inputcelsius").value);
-// //     const farenheit = parseFloat(document.getElementById("Inputfarenheit").value)
-// //     if (celsius !=0) {
-// //         convertirCelsiusaFarenheit(celsius)}
+document.addEventListener("DOMContentLoaded", function(){
+    let botonConvC = document.querySelector("#botonConvC")
+    botonConvC.addEventListener("click", imprimirFarenheitaC)
+  })
+  document.addEventListener("DOMContentLoaded", function(){
+    let botonConvF = document.querySelector("#botonConvF")
+    botonConvF.addEventListener("click", imprimirCelsiusaF)
+  })
